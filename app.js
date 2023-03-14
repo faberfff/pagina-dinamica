3d6643d92a8457c98603d9067c665e80266a79f5
faberfff <faverfff@gmail.com>
2023-03-13 21:34:27 -0500

    Borrado de comentarios de file app.js

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true })
       .then(() => console.log('MongoDB connected', mongodb))
       .catch(err => console.log(err));
 
-
 const app = express();
 
 // view engine setup
@@ -28,37 +27,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/index.routes'));
-app.use('/', require('./routes/game-cam.routes'));
-
-app.use('/gameprueba', require('./routes/gameprueba.routes'));
-//app.use('/', require('./routes/gamebyid.routes'));
-
-
 app.use('/game', require('./routes/game.routes'));
 app.use('/create-game.routes', require('./routes/create-game.routes'));
-//app.use('/games', require('./routes/games'));
 
-
-/*app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
-app.use('/user', require('./routes/create-user'));
-app.use('/user', require('./routes/delete-user'));
-app.use('/user', require('./routes/total-change-user'));
-app.use('/user', require('./routes/partial-change-user'));*/
-
-// catch 404 and forward to error handler
-/*app.use(function(req, res, next) {
-  next(createError(404));
-});*/
-
-// error handler
-/*app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});*/
 
 module.exports = app;
